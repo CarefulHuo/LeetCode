43bf621b5b820f8c94d96070e7dbfe7d753607a0
huoweiye <huoweiye@newcapec.net>
2024-11-19 09:26:47 +0800

    LeetCode题目： 215.数组中的第K个最大元素，优化三向切分法的快速排序，通过了

--- src/main/java/cf/carefulhuo/leetcode/sort/FindKthLargest.java
+++ src/main/java/cf/carefulhuo/leetcode/sort/FindKthLargest.java
@@ -1,6 +1,8 @@
 package cf.carefulhuo.leetcode.sort;
 
 
+import java.util.Arrays;
+
 /**
  * 215.数组中的第K个最大元素
  */
@@ -9,7 +11,7 @@ public class FindKthLargest {
         int[] nums = {3, 2, 3, 1, 2, 4, 5, 5, 6};
 //        int[] nums = {3, 2, 1, 5, 6, 4};
         int k = 4;
-        System.out.println(findKthLargest3(nums, k));
+        System.out.println(findKthLargest4(nums, k));
     }
 
     /**
@@ -26,7 +28,7 @@ public class FindKthLargest {
 
     private static void quickSort(int[] nums, int p, int r) {
         if (p >= r) return;
-        int q = partition(nums, p, r);
+        int q = partitionDesc(nums, p, r);
         quickSort(nums, p, q - 1);
         quickSort(nums, q + 1, r);
     }
@@ -39,7 +41,7 @@ public class FindKthLargest {
      * @param r
      * @return
      */
-    private static int partition(int[] nums, int p, int r) {
+    private static int partitionDesc(int[] nums, int p, int r) {
         int pivot = nums[r];
         int i = p;
         for (int j = p; j < r; j++) {
@@ -56,6 +58,14 @@ public class FindKthLargest {
         return i;
     }
 
+    /**
+     * 快速，但是只递归到要找的元素所在的位置，而不是对整个数组排序后，只返回第k大的元素
+     * 此方法也不可行，最后一个测试用例超时
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
     public static int findKthLargest3(int[] nums, int k) {
         return quickSort2(nums, 0, nums.length - 1, k);
     }
@@ -63,7 +73,7 @@ public class FindKthLargest {
     private static int quickSort2(int[] nums, int p, int r, int k) {
         if (p > r) return 0;
         // 找到 q，此时 q 左边比 q 大，q 右边比 q 小
-        int q = partition(nums, p, r);
+        int q = partitionDesc(nums, p, r);
         if ((q + 1) == k) {
             // 此处返回的正好是第 K 大的元素
             return nums[q];
@@ -74,6 +84,83 @@ public class FindKthLargest {
             // 说明要找的元素在左半边，继续递归
             return quickSort2(nums, p, q - 1, k);
         }
+    }
+
+    /**
+     * 三向切分法，快速排序
+     * 先使用三向切分法对数据进行预处理，将小于切割点的放到左边，等于切割点的放到中间，大于切割点的放到右边。
+     * 再对数据进行快速排序，只不过是对两个数据组排序，而不是对整个数组进行排序。
+     */
+    public static int findKthLargest4(int[] nums, int k) {
+        quickSort3(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+        return nums[nums.length - k];
+    }
+
+    private static void quickSort3(int[] nums, int left, int right) {
+        if (left >= right) return;
+        int l = left;
+        int k = left + 1;
+        int r = right;
+        int pivot = nums[l];
+
+        while (k <= r) {
+            if (nums[k] < pivot) {
+                int tmp = nums[k];
+                nums[k] = nums[l];
+                nums[l] = tmp;
+                l++;
+                k++;
+            } else if (nums[k] == pivot) {
+                k++;
+                // 此时的 nums[k] > pivot
+            } else {
+                // nums[r] < pivot，需要与 nums[l] 元素交换
+                if (nums[r] < pivot) {
+                    int tmp = nums[l];
+                    nums[l] = nums[r];
+                    nums[r] = nums[k];
+                    nums[k] = tmp;
+                    r--;
+                    k++;
+                    l++;
+                } else if (nums[r] > pivot) {
+                    r--;
+                } else {
+                    int tmp = nums[k];
+                    nums[k] = nums[r];
+                    nums[r] = tmp;
+                    r--;
+                    k++;
+                }
+            }
+        }
+
+        quickSelect(nums, left, l - 1);
+        quickSelect(nums, r + 1, right);
+    }
+
+    public static void quickSelect(int[] nums, int p, int r) {
+        if (p >= r) return;
+        int q = partitionAsc(nums, p, r);
+        quickSelect(nums, p, q - 1);
+        quickSelect(nums, q + 1, r);
+    }
+
+    private static int partitionAsc(int[] nums, int p, int r) {
+        int pivot = nums[r];
+        int i = p;
+        for (int j = p; j < r; j++) {
+            if (nums[j] < pivot){
+                int tmp = nums[j];
+                nums[j] = nums[i];
+                nums[i++] = tmp;
+            }
+        }
+        int tmp = nums[i];
+        nums[i] = nums[r];
+        nums[r] = tmp;
+        return i;
     }
 
     /**
